c527bb3d7665845c374950daebf9bdd5942c38d1
Gwen-83 <gwenael.drouet@gmail.com>
2025-06-02 22:05:08 +0200

    amélioration start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -4,5 +4,8 @@
 echo "Attente de la base de données..."
 sleep 5
 
-# Lancer Gunicorn avec le bon module
-exec gunicorn run:app --bind 0.0.0.0:$PORT --worker-class gevent
+# Lancer les migrations de la base de données
+flask db upgrade
+
+# Lancer Gunicorn avec le bon module, 1 worker, timeout 60 secondes, et gevent
+exec gunicorn run:app --bind 0.0.0.0:$PORT --worker-class gevent --workers 1 --timeout 60
